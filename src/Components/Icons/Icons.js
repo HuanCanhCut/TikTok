ab36951a01d2e4d9377c85b30b6c68eb56f9dec9
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-10-16 11:28:38 +0700

    Scroll Into View

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -270,3 +270,21 @@ export const Muted = ({ width = '32', height = '32', className }) => (
         ></path>
     </svg>
 )
+
+export const GoToTop = ({ width = '32', height = '32', className }) => (
+    <svg
+        width={width}
+        height={height}
+        className={className}
+        viewBox="0 0 48 48"
+        fill="#FFF"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M22.1086 20.3412C23.1028 19.2196 24.8972 19.2196 25.8914 20.3412L42.8955 39.5236C44.2806 41.0861 43.1324 43.5 41.004 43.5L6.99596 43.5C4.86764 43.5 3.71945 41.0861 5.10454 39.5235L22.1086 20.3412Z"
+        ></path>
+        <path d="M4.5 7.5C4.5 5.84315 5.84315 4.5 7.5 4.5L40.5 4.5C42.1569 4.5 43.5 5.84315 43.5 7.5C43.5 9.15685 42.1569 10.5 40.5 10.5L7.5 10.5C5.84315 10.5 4.5 9.15685 4.5 7.5Z"></path>
+    </svg>
+)
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useEffect, useState, useRef } from 'react'
 import classNames from 'classnames/bind'
 import Modal from 'react-modal'
+import { faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Virtuoso } from 'react-virtuoso'
 
 import style from './Home.module.scss'
 import Video from '~/layouts/components/Video'
 import * as videoService from '~/services/videoService'
-import { Virtuoso } from 'react-virtuoso'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { GoToTop } from '~/Components/Icons'
+
 const cx = classNames.bind(style)
 
 const INIT_PAGE = 1
@@ -15,6 +17,8 @@ function Home() {
     const [videos, setVideos] = useState([])
     const [page, setPage] = useState(INIT_PAGE)
     const [isOpen, setIsOpen] = useState(true)
+    const [goToTop, setGoToTop] = useState(false)
+    const headerIntoview = useRef()
 
     const overlayStyle = {
         position: 'fixed',
@@ -27,6 +31,13 @@ function Home() {
 
     const closeModal = () => {
         setIsOpen(false)
+    }
+
+    const handleGoToTop = () => {
+        headerIntoview.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+        })
     }
 
     useEffect(() => {
@@ -46,6 +57,19 @@ function Home() {
         })()
     }, [page])
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop
+            setGoToTop(scrollTop > 10)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
         <div className={cx('wrapper')}>
             {isOpen || (
@@ -59,6 +83,9 @@ function Home() {
                         return <Video key={index} data={item} />
                     }}
                     components={{
+                        Header: () => {
+                            return <div className={cx('header-intoview')} ref={headerIntoview}></div>
+                        },
                         Footer: () => {
                             return (
                                 <button className={cx('load-more')}>
@@ -70,6 +97,12 @@ function Home() {
                 />
             )}
 
+            {goToTop && (
+                <button className={cx('go-to-top')} onClick={handleGoToTop}>
+                    <GoToTop width="16px" height="16px" />
+                </button>
+            )}
+
             {isOpen && (
                 <div style={overlayStyle}>
                     <Modal
@@ -77,6 +110,7 @@ function Home() {
                         onRequestClose={closeModal}
                         overlayClassName={cx('overlay')}
                         className={cx('ads')}
+                        ariaHideApp={false}
                     >
                         <button className={cx('des')}>
                             Do chính sách của google tắt mọi âm thanh khi web đc load mà chưa có tương tác của người
