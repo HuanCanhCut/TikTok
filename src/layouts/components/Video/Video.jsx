d2582643c0155b490d92c627ba77b1cefe1169c5
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-12-03 18:10:42 +0700

    Restructuring the call videos section

--- src/layouts/components/Video/Video.jsx
+++ src/layouts/components/Video/Video.jsx
@@ -1,17 +1,84 @@
 import PropTypes from 'prop-types'
 import classNames from 'classnames/bind'
-import { memo, useEffect } from 'react'
+import { memo, useEffect, useRef, useContext, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actions } from '~/redux'
+import { Virtuoso } from 'react-virtuoso'
 
+import { currentUserData } from '~/App'
+import * as videoService from '~/services/videoService'
 import style from './Video.module.scss'
 import Header from './Header'
 import VideoItem from '~/Components/VideoItem'
+import AccountLoading from '~/Components/AccountLoading'
 
 const cx = classNames.bind(style)
 
-function Video({ data, videos, virtuosoRef, setFocusedIndex, focusedIndex }) {
+const TOTAL_PAGES_KEY = 'totalVideoPages'
+const TOTAL_PAGES_VIDEO = JSON.parse(localStorage.getItem(TOTAL_PAGES_KEY))
+function Video({ type }) {
+    const dispatch = useDispatch()
+    const currentUser = useContext(currentUserData)
+    const accessToken = currentUser && currentUser.meta.token
+
+    const virtuosoRef = useRef()
+
+    const [videos, setVideos] = useState([])
+    const [page, setPage] = useState(() => {
+        return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
+    })
+    const [pageIndexes, setPageIndexes] = useState(JSON.parse(localStorage.getItem('pageIndexes')) ?? [])
+    const [focusedIndex, setFocusedIndex] = useState(0)
+
     const scrollToIndex = (index) => {
         virtuosoRef.current.scrollToIndex({ index: index, align: 'center', behavior: 'smooth' })
     }
+
+    const handleScroll = () => {
+        if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+            setPage(() => {
+                do {
+                    return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
+                } while (pageIndexes.includes(Math.floor(Math.random() * TOTAL_PAGES_VIDEO)))
+            })
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        ;(async () => {
+            try {
+                const response = await videoService.getVideo({
+                    type,
+                    page: page,
+                    accessToken,
+                })
+
+                localStorage.setItem(TOTAL_PAGES_KEY, JSON.stringify(response.meta.pagination.total_pages))
+
+                setVideos((prev) => {
+                    return [...prev, ...response.data]
+                })
+
+                setPageIndexes((prev) => {
+                    return [...prev, page]
+                })
+
+                dispatch(actions.temporaryFollowed([]))
+                dispatch(actions.temporaryUnFollowed([]))
+
+                localStorage.setItem('pageIndexes', JSON.stringify(pageIndexes))
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page])
 
     useEffect(() => {
         const handleClickKeys = (e) => {
@@ -44,14 +111,37 @@ function Video({ data, videos, virtuosoRef, setFocusedIndex, focusedIndex }) {
 
     return (
         <div className={cx('wrapper')}>
-            <Header data={data} />
-            <VideoItem video={data} />
+            <Virtuoso
+                ref={virtuosoRef}
+                data={videos}
+                useWindowScroll
+                endReached={() => {
+                    setPage(() => {
+                        do {
+                            return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
+                        } while (pageIndexes.includes(Math.floor(Math.random() * TOTAL_PAGES_VIDEO)))
+                    })
+                }}
+                itemContent={(index, item) => {
+                    return (
+                        <div key={index}>
+                            <Header data={item} />
+                            <VideoItem video={item} />
+                        </div>
+                    )
+                }}
+                components={{
+                    Footer: () => {
+                        return <AccountLoading big />
+                    },
+                }}
+            />
         </div>
     )
 }
 
 Video.propTypes = {
-    data: PropTypes.object.isRequired,
+    type: PropTypes.string.isRequired,
 }
 
 export default memo(Video)
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,16 @@
-import { memo, useEffect, useState, useCallback, useRef, useContext } from 'react'
+import { memo, useState, useCallback } from 'react'
 import classNames from 'classnames/bind'
 import Modal from 'react-modal'
-import images from '~/assets/images'
-import { currentUserData } from '~/App'
-import { Virtuoso } from 'react-virtuoso'
-import { useDispatch } from 'react-redux'
 
-import { actions } from '~/redux'
-import AccountLoading from '~/Components/AccountLoading'
-import style from './Home.module.scss'
+import images from '~/assets/images'
 import Video from '~/layouts/components/Video'
-import * as videoService from '~/services/videoService'
+import style from './Home.module.scss'
 import Notification from '~/Components/Notification'
 
 const cx = classNames.bind(style)
 
-const TOTAL_PAGES_KEY = 'totalVideoPages'
-const TOTAL_PAGES_VIDEO = JSON.parse(localStorage.getItem(TOTAL_PAGES_KEY))
-
 function Home() {
-    const dispatch = useDispatch()
-    const currentUser = useContext(currentUserData)
-    const accessToken = currentUser && currentUser.meta.token
-
-    const virtuosoRef = useRef()
-    const [videos, setVideos] = useState([])
-    const [page, setPage] = useState(() => {
-        return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
-    })
     const [modalIsOpen, setModalIsOpen] = useState(JSON.parse(localStorage.getItem('firstNotification')) ?? true)
-    const [pageIndexes, setPageIndexes] = useState(JSON.parse(localStorage.getItem('pageIndexes')) ?? [])
-    const [focusedIndex, setFocusedIndex] = useState(0)
 
     const closeModal = useCallback(() => {
         localStorage.setItem('firstNotification', JSON.stringify(false))
@@ -43,52 +23,6 @@ function Home() {
             'Chuyện kể rằng, do chính sách của google tắt mọi âm thanh tự phát khi mà chưa có tương tác của người dùng, mà admin lại chưa nghĩ ra ý tưởng để pass qua cái đó, nên admin quyết định làm cái thông báo này để người dùng tương tác trước, mong thí chủ thông cảm cho Admin.',
         path: images.notificationCat,
     }
-
-    useEffect(() => {
-        ;(async () => {
-            try {
-                const response = await videoService.getVideo({
-                    type: 'for-you',
-                    page: page,
-                    accessToken,
-                })
-
-                localStorage.setItem(TOTAL_PAGES_KEY, JSON.stringify(response.meta.pagination.total_pages))
-
-                setVideos((prev) => {
-                    return [...prev, ...response.data]
-                })
-
-                setPageIndexes((prev) => {
-                    return [...prev, page]
-                })
-
-                dispatch(actions.temporaryFollowed([]))
-                dispatch(actions.temporaryUnFollowed([]))
-
-                localStorage.setItem('pageIndexes', JSON.stringify(pageIndexes))
-            } catch (error) {
-                console.log(error)
-            }
-        })()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [page])
-
-    const handleScroll = () => {
-        if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-            setPage(() => {
-                do {
-                    return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
-                } while (pageIndexes.includes(Math.floor(Math.random() * TOTAL_PAGES_VIDEO)))
-            })
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     return (
         <>
@@ -108,36 +42,8 @@ function Home() {
                     />
                 </Modal>
             ) : (
-                <div className={cx('wrapper')} tabIndex={10}>
-                    <Virtuoso
-                        ref={virtuosoRef}
-                        data={videos}
-                        useWindowScroll
-                        endReached={() => {
-                            setPage(() => {
-                                do {
-                                    return Math.floor(Math.random() * TOTAL_PAGES_VIDEO)
-                                } while (pageIndexes.includes(Math.floor(Math.random() * TOTAL_PAGES_VIDEO)))
-                            })
-                        }}
-                        itemContent={(index, item) => {
-                            return (
-                                <Video
-                                    key={index}
-                                    data={item}
-                                    videos={videos}
-                                    virtuosoRef={virtuosoRef}
-                                    setFocusedIndex={setFocusedIndex}
-                                    focusedIndex={focusedIndex}
-                                />
-                            )
-                        }}
-                        components={{
-                            Footer: () => {
-                                return <AccountLoading big />
-                            },
-                        }}
-                    />
+                <div className={cx('wrapper')}>
+                    <Video type="for-you" />
                 </div>
             )}
         </>
