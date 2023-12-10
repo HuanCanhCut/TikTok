a8ef2a5435f6288f150562908b2283d86c35788d
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-12-10 22:30:17 +0700

    test

--- src/layouts/components/Authen/Auth.jsx
+++ src/layouts/components/Authen/Auth.jsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind'
 import style from './Login.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { memo } from 'react'
 import Modal from 'react-modal'
 import { motion, AnimatePresence } from 'framer-motion'
@@ -11,20 +11,75 @@ import { useDispatch } from 'react-redux'
 import { actions } from '~/redux'
 
 import { useState } from 'react'
+import * as Auth from '~/services/authService'
+import Button from '~/Components/Button'
 import LoginWith from './LoginWith/LoginWith'
 import Policy from './Policy'
 import Input from './Input'
+import config from '~/config'
 import useDarkMode from '~/hooks/useDarkMode'
 
 const cx = classNames.bind(style)
 
 function Authen() {
     const dispatch = useDispatch()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isValid, setIsValid] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [signUp, setSignUp] = useState(false)
+
     const modalIsOpen = useSelector(openAuth)
+
+    const setLocalStorage = (key, value) => {
+        const Action = localStorage.setItem(key, JSON.stringify(value))
+        window.location.reload()
+        return Action
+    }
+
+    const handleSubmitLogin = async () => {
+        try {
+            // call API when signUp
+            if (signUp) {
+                setLoading(true)
+                const response = await Auth.signUp(email, password)
+                setLoading(false)
+                if (response.meta && response.meta.token) {
+                    setIsValid(false)
+                    setLocalStorage('user', response)
+                } else {
+                    setIsValid(true)
+                }
+            }
+            // call API  when log in
+            else {
+                setLoading(true)
+                const response = await Auth.login(email, password)
+                setLoading(false)
+                if (response.meta && response.meta.token) {
+                    setIsValid(false)
+                    setLocalStorage('user', response)
+                } else {
+                    setIsValid(true)
+                }
+            }
+        } catch (error) {
+            setIsValid(true)
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        switch (e.key) {
+            case 'Enter':
+                handleSubmitLogin()
+                break
+            default:
+        }
+    }
 
     const handleLoginOptions = () => {
         setSignUp(signUp ? false : true)
+        setIsValid(false)
     }
 
     return (
@@ -48,6 +103,7 @@ function Authen() {
                         className={cx('wrapper', {
                             darkMode: useDarkMode(),
                         })}
+                        onKeyDown={handleKeyDown}
                     >
                         <button
                             className={cx('close')}
@@ -62,7 +118,24 @@ function Authen() {
                         </header>
 
                         <div className={cx('body')}>
-                            <Input signUp={signUp} />
+                            <Input setEmail={setEmail} setPassword={setPassword} email={email} password={password} />
+
+                            <span className={cx('invalid-password')}>
+                                {isValid && !signUp && `Username or password doesn't match our records. Try again.`}
+                                {signUp && isValid && 'Sign up isValid. Try again'}
+                            </span>
+
+                            <span className={cx('forgot-password')}>Forgot Password?</span>
+
+                            <Button
+                                to={config.routes.home}
+                                primary
+                                onClick={handleSubmitLogin}
+                                className={cx('login-btn')}
+                            >
+                                {loading || <span className={cx('login')}>Login</span>}
+                                {loading && <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />}
+                            </Button>
 
                             <div className={cx('separator')}>
                                 <span className={cx('separator-line')}></span>
